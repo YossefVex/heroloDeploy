1090a6d09630fae5023e4a08f1e3257239606371
Yossef <7773783@gmail.com>
2018-11-05 18:47:37 +0200

    first commit

---
+++ precache-manifest.3cd7aa4a89763bd60ec751a0d4695343.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/heroloDeploy/robots.txt"
+  },
+  {
+    "revision": "62b61a24cd24700da960",
+    "url": "/heroloDeploy/js/chunk-vendors.e778073d.js"
+  },
+  {
+    "revision": "2f9f3a5e170d8927cab8",
+    "url": "/heroloDeploy/js/app.969527bc.js"
+  },
+  {
+    "revision": "225f2b8cdf5ccc2e4954",
+    "url": "/heroloDeploy/js/about.be8eec35.js"
+  },
+  {
+    "revision": "561b9111991c975d7de57c0e400380e4",
+    "url": "/heroloDeploy/index.html"
+  },
+  {
+    "revision": "81bf03dfd3c9cb0a61bce8cc6d67040d",
+    "url": "/heroloDeploy/img/icons.81bf03df.png"
+  },
+  {
+    "revision": "2f9f3a5e170d8927cab8",
+    "url": "/heroloDeploy/css/app.8bf6e290.css"
+  },
+  {
+    "revision": "225f2b8cdf5ccc2e4954",
+    "url": "/heroloDeploy/css/about.5daaa489.css"
+  }
+];
